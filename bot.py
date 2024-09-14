5c1786c5dfdef014cf8496112c368838a0790131
aparovkl <festorandlemos@gmail.com>
2024-09-14 16:51:43 +0300

    Discord bot
    This is my discord bot

---
+++ bot.py
@@ -0,0 +1,51 @@
+from settings import settings
+import discord
+# import * - это тоже самое, что перечислить все файлы
+from bot_logic import *
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+
+# Когда бот будет готов, он напишет в консоли свое название!
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+# Когда бот будет получать сообщение, он будет отправлять в этот же канал какие-то сообщения!
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Привет! Я бот!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send("Я не понимаю такую команду!")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await guild.system_channel.send(to_send)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+client.run(settings["TOKEN"])
